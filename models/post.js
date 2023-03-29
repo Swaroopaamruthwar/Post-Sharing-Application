05aa1c11c2051e7eab70df248a8cf8b65d33066a
Swaroopaamruthwar <aswaroopa2806@gmail.com>
2023-03-29 15:16:05 +0530

    added files

---
+++ models/post.js
@@ -0,0 +1,24 @@
+// Define the post schema
+const mongoose = require("mongoose");
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+let Post = mongoose.model("Post", postSchema);
+module.exports = Post;
---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+// Define the user schema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+let User = mongoose.model("User", userSchema);
+module.exports = User;
